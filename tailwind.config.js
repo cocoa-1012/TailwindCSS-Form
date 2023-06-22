e73b631eb22d3816180c226e78cb8fd2ef8e1e31
cocoa-1012 <cocoa.pandaok@gmail.com>
2023-06-21 20:37:23 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main': "#EDF8FF"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
